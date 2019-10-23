6196a33c71ecf11ce664f06735b611a78c1174cd
PaolaHub <paola.tapiagarcia@gmail.com>
2019-10-23 23:38:40 +0200

    Seccion 12 terminada

--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+module.exports.CLIENT_ID = '275478591728-sdmersaqc5agrmg4712emf73orj2drp1.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -3,10 +3,120 @@ var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
 var SEED = require('../config/config').SEED;
+var CLIENT_ID = require('../config/config').CLIENT_ID;
 
 var app = express();
 
 var Usuario = require('../models/usuario');
+
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+// =================================
+// Autenticación de Google
+// =================================
+
+// Google recomienda que las peticiones se hagan por POST
+
+app.post('/google', async(req, res, next) => {
+
+    var token = req.body.token;
+
+    var googleUser = await verify(token).catch((error) => {
+        res.status(402).json({
+            ok: false,
+            mensaje: 'Token no válido',
+            errors: error
+        });
+    });
+
+    Usuario.findOne({ email: googleUser.email }, (error, usuarioDB) => {
+        if (error) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al buscar usuarios',
+                errors: error
+            });
+        }
+
+        if (usuarioDB) {
+
+            if (!usuarioDB.google) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe usar su autenticación normal',
+                    errors: error
+                });
+            } else {
+                // Generar un nuevo token y verificarlo
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+            }
+        } else {
+            // El usuario no existe hay que crearlo.
+            var usuario = new Usuario({
+                nombre: googleUser.nombre,
+                email: googleUser.email,
+                img: googleUser.img,
+                google: true,
+                password: ':)'
+            });
+
+            usuario.save((error, usuarioDB) => {
+
+                if (error) {
+                    return response.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al crear usuarios',
+                        errors: error
+                    });
+                }
+                // Generar un nuevo token y verificarlo
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+            });
+        }
+    });
+});
+
+// Volvemos a la documentación de google
+
+
+
+// =================================
+// Autenticación normal
+// =================================
 
 app.post('/', (request, response, next) => {
 
